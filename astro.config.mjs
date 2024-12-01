219421f8b52f750308c12e3e072ecb8c60f1bb13
Borris <borris.wiria@gmail.com>
2024-12-01 23:36:21 +0400

    build static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,20 @@ import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
 import keystatic from '@keystatic/astro'
 import tailwind from '@astrojs/tailwind'
-
+import { loadEnv } from 'vite'
+const { ASTRO_OUTPUT_MODE, ASTRO_USE_NETLIFY_ADAPTER } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ''
+)
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), tailwind()],
+  integrations: [
+    react(),
+    markdoc(),
+    ASTRO_OUTPUT_MODE === 'static' ? undefined : keystatic(),
+    tailwind(),
+  ],
 
   i18n: {
     locales: ['en', 'es'],
